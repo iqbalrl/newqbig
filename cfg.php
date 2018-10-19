69270a8f429866d2e2532740f2c6dd10d195a076
iqbalrl <ibaalrl15@yahoo.com>
2018-10-19 17:00:05 +0700

    terbaru

---
+++ cfg.php
@@ -0,0 +1,56 @@
+<?php
+$android_id = "9421fff6374fbc57";
+$deviceCode = "865815036619885";
+$device_ip  = "114.125.215.30";
+$token = "3b9eCUgf7CEKqfeYWeDN7dlB2dtZ4JyEmphLLLpiHDhUiI5MwWXK3YB57I6_IvGQCY9cjzB3PY7-YN0";
+$tk = "ACF_ZIf3xb5N04un0E9vR2xcr3xS8xEsqvZxdHRodw";
+$uuid = "934e0f96d6924a8ab9c8f7a0546012a2";
+$userid = "7053695";
+$sign = "42f92fcf9e96d2e1c76617b4e1bbd06b";
+
+####################################################
+#|note:
+#| *untuk aktifitas nuyul multi akun
+#|  gunakan andoid_id yang berbeda setiap akun
+#|  gunakan devicecode (imei) yang berbeda setiap akun
+#|  gunakan device_ip yang berbeda setiap akun
+#| *untuk settingan lainya wajib sama 
+#|  dengan data dari packet capture
+#|
+####################################################
+#| cara mengaktifkan multi akun
+#| duplikat data di atas dari anroid_id => sign
+#| buat file baru dengan nama bebas terserah kamu
+#| aktifkan skrip di bawah dngn menghapus tanda pagar
+#| jika skrip di aktifkan maka hapus data di atas 
+#|#####################################################
+#| hapus pagar di bawah untuk mengaktifkan skrip
+
+# include(readline("masukan config: "));
+$deviceCode = "8695450xxxxxxxx";
+$device_ip  = "202.80xxxxxxxxx";
+$token = "7d62FfijHQIvn0CnZLYJ8xxxxxxxxxxxxxxxxxx";
+$tk = "ACGQr00V8VRJ14emgw7yUOQ5xxxxxxxxxxxxxxxxxx";
+$uuid = "e1ac1cfe11f44xxxxxxxxxx";
+$userid = "71xxxxxxx";
+$sign = "0f72bab003xxxxxxxxxxxxx";
+
+####################################################
+#|note:
+#| *untuk aktifitas nuyul multi akun
+#|  gunakan andoid_id yang berbeda setiap akun
+#|  gunakan devicecode (imei) yang berbeda setiap akun
+#|  gunakan device_ip yang berbeda setiap akun
+#| *untuk settingan lainya wajib sama 
+#|  dengan data dari packet capture
+#|
+####################################################
+#| cara mengaktifkan multi akun
+#| duplikat data di atas dari anroid_id => sign
+#| buat file baru dengan nama bebas terserah kamu
+#| aktifkan skrip di bawah dngn menghapus tanda pagar
+#| jika skrip di aktifkan maka hapus data di atas 
+#|#####################################################
+#| hapus pagar di bawah untuk mengaktifkan skrip
+
+# 
